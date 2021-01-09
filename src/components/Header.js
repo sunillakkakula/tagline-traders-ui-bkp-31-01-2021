f6f377f834d306d7ead1941d27581b235e00abd6
= <=>
2021-01-09 20:20:32 +0530

    UI Custom Changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,7 +18,12 @@ const Header = () => {
 
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Navbar
+        style={{ background: "#800000" }}
+        variant="dark"
+        expand="lg"
+        collapseOnSelect
+      >
         <Container>
           <LinkContainer to="/">
             <Navbar.Brand>Tagline Traders</Navbar.Brand>
